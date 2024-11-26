76cfd50837ce407ebe28a55a660e250744205d05
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-26 15:35:31 -0500

    26 1535 Nov 24 express_server.js

--- express_server.js
+++ express_server.js
@@ -9,8 +9,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT,() => {
@@ -19,4 +24,9 @@ app.listen(PORT,() => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
