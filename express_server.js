ea5e352bc2ee55524c59d74a6178af3b88e5124c
Javin Leslie Lau <javinlau21@gmail.com>
2024-11-30 19:12:52 -0500

    30 1912 Nov 24, express_server.js

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; //default port 8080
 
 app.set("view engine", "ejs");
 
+//add this middleware to parse URL-encoded payloads
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -13,9 +16,28 @@ const urlDatabase = {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(); // generate a new short URL
+  const longURL = req.body.longURL; // extract the long URL from the form data
+
+  // add the new short URL and its corresponding long URL to the database
+  urlDatabase[shortURL] = longURL;
+  
+  console.log(`Short URL: ${shortURL}, Long URL: ${longURL}`); //log the mapping
+  res.redirect(`/urls/${shortURL}`); // redirect to the short URL's page
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -29,7 +51,7 @@ app.get("/urls/:id", (req, res) => {
   }
 
   const templateVars = { id, longURL }; //pass ID and longURL to the template
-  res.render("urls_show",templateVars); //render the template
+  res.render("urls_show", templateVars); //render the template
 });
 
 app.get("/hello", (req, res) => {
@@ -49,3 +71,8 @@ const urls = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// app.post("/urls", (req, res) => {
+//   console.log(req.body); // Log the POST request body to the console
+//   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+// });
